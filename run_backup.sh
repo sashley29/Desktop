40f1c46dcaaf8216e952882e001918f9e21ed2c8
shawn <shawn@home-desktop>
2019-12-27 15:31:33 -0600

    move python code to src folder

--- run_backup.sh
+++ run_backup.sh
@@ -2,5 +2,5 @@
 export GOOGLE_APPLICATION_CREDENTIALS="/home/shawn/Projects/desktop_backup/env/secrets/Playground-15ed10c8f21e.json"
 BASE=/home/shawn/Projects/desktop_backup/env
 
-python $BASE/DesktopBackup.py /home $BASE/working
+python $BASE/src/DesktopBackup.py /home $BASE/working
 
