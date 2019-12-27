2d667fcc54e6086f13328b3a58350457d5adb47a
shawn <shawn@home-desktop>
2019-12-27 15:28:30 -0600

    move credentials to folder and update gitignore

--- run_backup.sh
+++ run_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export GOOGLE_APPLICATION_CREDENTIALS="/home/shawn/Projects/desktop_backup/env/Playground-15ed10c8f21e.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/shawn/Projects/desktop_backup/env/secrets/Playground-15ed10c8f21e.json"
 BASE=/home/shawn/Projects/desktop_backup/env
 
 python $BASE/DesktopBackup.py /home $BASE/working
