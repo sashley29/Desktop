f43b6d64b066868eac1c4f629a39cf4e0af7930a
shawn <shawn@home-desktop>
2019-12-27 15:32:45 -0600

    move shell scripts to folder

---
+++ scripts/run_backup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export GOOGLE_APPLICATION_CREDENTIALS="/home/shawn/Projects/desktop_backup/env/secrets/Playground-15ed10c8f21e.json"
+BASE=/home/shawn/Projects/desktop_backup/env
+
+python $BASE/src/DesktopBackup.py /home $BASE/working
+
