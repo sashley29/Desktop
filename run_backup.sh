0d82d96fb06b28ff092d9c561f40f332c0358ae8
shawn <shawn@home-desktop>
2019-12-27 15:47:04 -0600

    catch up moved files

--- run_backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export GOOGLE_APPLICATION_CREDENTIALS="/home/shawn/Projects/desktop_backup/env/secrets/Playground-15ed10c8f21e.json"
-BASE=/home/shawn/Projects/desktop_backup/env
-
-python $BASE/src/DesktopBackup.py /home $BASE/working
-
