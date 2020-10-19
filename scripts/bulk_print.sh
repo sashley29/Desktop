6dedde6dc2a4a5b17d0cfe791b5f2399e8343299
Shawn <sashley29@gmail.com>
2020-10-18 22:48:25 -0500

    added prompt and default to color

--- scripts/bulk_print.sh
+++ scripts/bulk_print.sh
@@ -14,14 +14,27 @@ then
 	exit 1
 fi
 
+if [ ! -d "$PRINT_DIR/archive" ]
+then
+	echo "Creating archive folder"
+	mkdir $PRINT_DIR/archive
+fi
+
+read -p 'Are you sure you want to bulk print (y/n)? ' USER_ANSWER
+if [ $USER_ANSWER = 'n' ]
+then
+	exit 1
+fi
+
 #set shell line separator variable to newlines only --- handles file names with whitespace
 OIFS="$IFS"
 IFS=$'\n'
 
-for FILE in `find $PRINT_DIR -type f`
+for FILE in `find $PRINT_DIR -type f -maxdepth 1`
 do 
 	echo "Sending file $FILE to printer."
-	lp -o ColorModel=KGray -o number-up=1 -o fit-to-page -d Photosmart_6510 $FILE
+	lp -o ColorModel=Color -o number-up=1 -o fit-to-page -d Photosmart_6510 $FILE
+	mv $FILE $PRINT_DIR/archive
 done
 
 IFS="$OIFS"
